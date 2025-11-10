4ca7669b72239bd8e430eff1d1bb0ec7fb287be1
Tulasi Shukla <tulsishuklag@gmail.com>
2025-11-10 17:14:57 +0530

    Update server.js

--- backend folder/server.js
+++ backend folder/server.js
@@ -23,9 +23,17 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI, { })
+// mongoose.connect(process.env.MONGO_URI, { })
+//   .then(() => {
+//     console.log("MongoDB connected");
+//     app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+//   })
+//   .catch(err => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    console.log("MongoDB connected ✅");
+    app.listen(PORT, "0.0.0.0", () =>
+      console.log(`Server running on port ${PORT}`)
+    );
   })
   .catch(err => console.error("MongoDB connection error:", err));
