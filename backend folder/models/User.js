3511bd9eefc44d86e8a009beb3f8c08f2e4347fb
Tulasi Shukla <tulsishuklag@gmail.com>
2025-11-12 12:46:08 +0530

    Update User.js

--- backend folder/models/User.js
+++ backend folder/models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email:    { type: String, required: true, unique: true },
-  password: { type: String, required: true } // bcrypt hashed
+  password: { type: String, required: true } 
 }, { timestamps: true });
 
 export default mongoose.model("User", userSchema);
