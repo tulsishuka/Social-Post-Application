c6a6dcf161cb280a6a235573825365287f175654
Tulasi Shukla <tulsishuklag@gmail.com>
2025-11-12 12:36:26 +0530

    Update server.js

--- backend folder/server.js
+++ backend folder/server.js
@@ -14,7 +14,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// static folder to serve uploaded images
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
@@ -23,12 +22,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
 const PORT = process.env.PORT || 5000;
-// mongoose.connect(process.env.MONGO_URI, { })
-//   .then(() => {
-//     console.log("MongoDB connected");
-//     app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-//   })
-//   .catch(err => console.error("MongoDB connection error:", err));
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected ✅");
